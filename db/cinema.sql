9f2857d96f2bd992b3d5e45cb94027095f6b8071
Rob Wilson <rob.wilson195@gmail.com>
2018-11-03 18:30:04 +0000

    Commented out changes relating to screenings pending review.

--- db/cinema.sql
+++ db/cinema.sql
@@ -14,15 +14,16 @@ CREATE TABLE customers (
   funds INT
 );
 
+-- CREATE TABLE screenings (
+--   id SERIAL4 PRIMARY KEY,
+--   film_id INT4 REFERENCES films(id) ON DELETE CASCADE,
+--   film_title TEXT REFERENCES films(title) ON DELETE CASCADE,
+--   show_time TEXT
+);
+
 CREATE TABLE tickets (
   id SERIAL4 PRIMARY KEY,
   customer_id INT4 REFERENCES customers(id) ON DELETE CASCADE,
-  film_id INT4 REFERENCES films(id) ON DELETE CASCADE
-);
-
-CREATE TABLE screenings (
-  id SERIAL4 PRIMARY KEY,
   film_id INT4 REFERENCES films(id) ON DELETE CASCADE,
-  film_title TEXT REFERENCES films(title) ON DELETE CASCADE,
-  show_time TEXT
+  -- screening_id INT4 REFERENCES screenings(id) ON DELETE CASCADE
 );
